591a5eb1f88c7814313c28fc7990c88e68a899ff
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2024-11-23 11:51:32 +0300

    hello world

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/temidaradev/golang-todo-app
+
+go 1.23.2
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+
+	fmt.Printf("Server is online on port %v \n", listenAddr)
+	http.ListenAndServe(listenAddr, r)
+}
