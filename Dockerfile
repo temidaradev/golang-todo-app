942ae171e0b5e550e333f5a78ffa3583f2befbe4
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2024-11-26 22:55:04 +0300

    help me

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,7 @@ WORKDIR /usr/src/app
 
 RUN go install github.com/air-verse/air@latest
 RUN go install github.com/a-h/templ/cmd/templ@latest
-RUN templ generate --watch --proxy=http://localhost:3000
 RUN air
-RUN npm install -D tailwindcss
 RUN make css
 
 COPY . .
--- database/database.go
+++ database/database.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/temidaradev/golang-todo-app/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -18,10 +19,16 @@ type DBI struct {
 var DB DBI
 
 func ConnectDB() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
 	dsn := fmt.Sprintf("host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC+3",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"))
+		os.Getenv("DB_NAME"),
+	)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
